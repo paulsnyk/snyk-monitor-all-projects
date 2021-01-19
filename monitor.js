c95eee6fd0ad637e29158b24d9cc5e45e09eeba0
paulsnyk <62235669+paulsnyk@users.noreply.github.com>
2021-01-19 18:01:02 +0000

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -1,35 +1,44 @@
-var util = require('util')
+const { promisify } = require('util');
+const { resolve } = require('path');
+const fs = require('fs');
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
 var exec = require('child_process').exec;
-var path = require('path');
-var fs	 = require('fs');
 
-function fromDir(startPath,filter){
+async function getFiles(dir) {
+  const subdirs = await readdir(dir);
+  const files = await Promise.all(subdirs.map(async (subdir) => {
+    const res = resolve(dir, subdir);
+    return (await stat(res)).isDirectory() ? getFiles(res) : res;
+  }));
+  return files.reduce((a, f) => a.concat(f), []);
+}
 
-    console.log('Starting from dir '+startPath+'/');
 
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
-        return;
-    }
+console.log('process agr', process.argv[3]);
 
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(files[i]);
-        if (stat.isDirectory()){
-            fromDir(filename,filter); //recurse
-        }
-        else if (filter.indexOf(filename)>=0) {
-            console.log('-- found: ',filename);
-			
-			exec("snyk monitor --file=" + filename + " --project-name=" + process.argv[3] + "/" + filename + " --remote-repo-url=" + process.argv[2], function(error, stdout, stderr) {
+fs.renameSync(".git", "temp.git");
+
+getFiles(__dirname)
+  .then(function(files){
+
+	files.forEach(function (file) {
+
+		if (file.includes('pom.xml')) {
+			var fileArray = file.split('\\');
+			var fileName = fileArray.slice(-1);
+			var relativeFilePath = file.replace(__dirname, '');
+			console.log(__dirname, fileName, file, relativeFilePath);
+
+			exec("snyk monitor --file=" + file + " --project-name=" + process.argv[3] + "-" + relativeFilePath + " --remote-repo-url=" + process.argv[2], function(error, stdout, stderr) {
 
 			});
 
         };
-    };
-};
 
-fs.renameSync(".git", "temp.git");
-fromDir('.',['pom.xml', 'package.json']);
-fs.renameSync("temp.git", ".git");
+	});
+	
+	fs.renameSync("temp.git", ".git");
+  })
+  .catch(e => console.error(e));
+  
